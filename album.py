dd045dc969f907a337cc3e43fa07e14daacb2fb8
z <z.cat88@outlook.com>
2025-07-14 23:54:36 -0400

    Fixes #1: Print local IP address during coming up

--- album.py
+++ album.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 import os
+import socket
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads/'
@@ -29,9 +30,23 @@ def upload():
         
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)  
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename) 
+    
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('8.8.8.8', 80))
+        local_ip = s.getsockname()
+    finally:
+        s.close()
+    return local_ip
 
 if __name__ == '__main__':
+   print(f"Serving Flask app '{app.name}'")
+   print(f"Debug mode: {app.debug}")
+   print(f"Local IP address: {get_local_ip()}")
+   print("WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.")
    app.run(host='0.0.0.0',port=8080,debug=True)
+   
 
 
