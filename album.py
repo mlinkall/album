179c147600f723b0e497352f1e5b1fdf2a47da9c
z <z.cat88@outlook.com>
2025-07-17 23:45:58 -0400

    Fixes #1: Print local IP address during coming up

--- album.py
+++ album.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, send_from_
 from werkzeug.utils import secure_filename
 import os
 import socket
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads/'
@@ -21,11 +22,28 @@ def upload():
     if request.method == 'POST':
         uploaded_files = request.files.getlist("file[]")
         filenames = []
+        today = datatime.now()
+        date_folder_name = today.strftime("%Y%m%d")
+        target_directory_path = os.path.join(app.config['UPLOAD_FOLDER'], data_folder_name)
+        
+        try:
+            os.makedirs(target_directory_path, exist_ok=True)
+        except OSError as e:
+            print(f"Error creating directory {target_directory_path}: {e}")
+            return "Server error: Could not create upload directory.", 500
+        
+        filenames = []
         for file in uploaded_files:
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                filenames.append(filename)
+                #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                #filenames.append(filename)
+                file_save_path = os.path.join(target_directory_path, filename)
+                file.save(file_save_path)
+                
+                relative_path_for_link = os.path.join(data_folder_name, filename)
+                filenames.append(relative_path_for_link)
+                
         return render_template('upload.html', filenames=filenames)
         
 @app.route('/uploads/<filename>')
